2c82f645df85d2d86a3f1a18d365427f62277068
valens200 <uwavalens2003@gmail.com>
2022-11-30 13:22:54 +0200

    [fix]:added the react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import Sidebar from './components/Sidebar'
-import BookMarkList from './components/BookMarkList'
-import CreateBookMark from './components/CreateBookMark'
 import Landing from './pages/Landing'
 import Home from './pages/Home'
 import Register from './pages/Register'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <BrowserRouter >
+    <Router >
       <Routes>
         <Route path="/dashboard" element={<Landing />}></Route>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
 
